e95e24c682e894c1eb8d26e3fa75e00d89bc3fee
vfa-tienld <tienld@gmail.com>
2024-01-06 19:01:39 +0700

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  components: [
+    {
+      path: "~/components",
+      pathPrefix: false,
+    },
+  ],
+  devServer: {
+    port: 1102,
+  },
   devtools: { enabled: false },
   css: [
     "vuetify/lib/styles/main.sass",
--- pages/admin-login/index.vue
+++ pages/admin-login/index.vue
@@ -1,19 +1,4 @@
 <template>
-    <v-sheet width="300" class="mx-auto">
-      <v-form fast-fail @submit.prevent>
-        <v-text-field
-        
-          label="First name"
-          
-        ></v-text-field>
-  
-        <v-text-field
-          
-          label="Last name"
-          
-        ></v-text-field>
-  
-        <v-btn type="submit" block class="mt-2">Submit</v-btn>
-      </v-form>
-    </v-sheet>
-  </template>
+  <Form />
+</template>
+<script lang="ts" setup></script>
