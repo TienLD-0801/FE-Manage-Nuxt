e856aa45a507563f6e80e18d23784da881cbcfa2
vfa-tienld <tienld@gmail.com>
2024-01-06 19:08:12 +0700

    update buttom

---
+++ components/Button/Button.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-sheet width="300" class="mx-auto">
+    <v-form fast-fail @submit.prevent>
+      <v-text-field label="First name" />
+      <v-text-field label="Last name" />
+      <v-btn type="submit" block class="mt -2" @click="handleClick"> Submit </v-btn>
+    </v-form>
+  </v-sheet>
+</template>
+
+<script lang="ts" setup>
+const handleClick = () => {
+  console.log("abc");
+};
+</script>
+
+<style></style>
