bd6118ba10bce70e2451e1d180f393183495eef1
vfa-tienld <tienld@gmail.com>
2024-01-06 17:52:10 +0700

    init nuxt

---
+++ pages/admin-login/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-sheet width="300" class="mx-auto">
+      <v-form fast-fail @submit.prevent>
+        <v-text-field
+        
+          label="First name"
+          
+        ></v-text-field>
+  
+        <v-text-field
+          
+          label="Last name"
+          
+        ></v-text-field>
+  
+        <v-btn type="submit" block class="mt-2">Submit</v-btn>
+      </v-form>
+    </v-sheet>
+  </template>
