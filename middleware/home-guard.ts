f8076b967de0dd341fd9aa65b4e17a6d2968dddc
vfa-tienld <tienld@gmail.com>
2024-01-07 15:27:17 +0700

    update

--- middleware/home-guard.ts
+++ middleware/home-guard.ts
@@ -1,7 +1,7 @@
 import { PATH_ROUTER } from '~/shared/constant/router';
 
 export default defineNuxtRouteMiddleware((to, from) => {
-  const isLogin = false;
+  const isLogin = true;
   if (!isLogin) {
     return navigateTo(PATH_ROUTER.login);
   }
