0eca7bc48cb57eea5f58ce538899d4b9b6c0719e
bang-se119 <bangduy.se119@gmail.com>
2024-01-08 23:28:43 +0700

    update ui main tab

---
+++ components/organisms/panel/chat-panel.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-main class="main-panel-container">
+    <MessageHeader />
+    <MessageInput />
+  </v-main>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
---
+++ components/templates/tab/main-tab.vue
@@ -0,0 +1,12 @@
+<template>
+  <ChatPanel v-if="navigatorTab.$state.currentTab === 'chat'" />
+  <SecurityPanel v-else-if="navigatorTab.$state.currentTab === 'security'" />
+  <UserPanel v-else-if="navigatorTab.$state.currentTab === 'users'" />
+  <SettingPanel v-else />
+</template>
+
+<script lang="ts" setup>
+const navigatorTab = useNavigatorTabStore();
+</script>
+
+<style lang="scss" scoped></style>
