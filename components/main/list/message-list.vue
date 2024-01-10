d19838b0ecf9b5b1bc6300c20ad4b8bf4a89a1a1
bang-se119 <bangduy.se119@gmail.com>
2024-01-10 10:13:56 +0700

    update ui message input

--- components/main/list/message-list.vue
+++ components/main/list/message-list.vue
@@ -45,14 +45,14 @@ const data = ref([
 .message-container {
   overflow: auto;
   height: 100%;
-  padding: 0 16px;
+  padding: 15px 25px;
   max-width: none;
   display: flex;
   flex-direction: column-reverse;
 }
 
 .message {
-  margin-bottom: 10px;
+  margin-bottom: 12px;
 }
 
 .message-right {
