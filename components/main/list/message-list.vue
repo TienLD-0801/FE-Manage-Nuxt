8df0e57f0ad7ae7d868a64ef8454c161de4db368
bang-se119 <bangduy.se119@gmail.com>
2024-01-13 09:53:45 +0700

    éc éc éc scroll infinite message list

--- components/main/list/message-list.vue
+++ components/main/list/message-list.vue
@@ -14,9 +14,25 @@
       :isSelf="item.user_id === $state.profile?.id"
     />
   </v-infinite-scroll>
+  <MessageInput
+    v-model="message"
+    @on-clear-message="clearMessage"
+    @on-send-message="sendMessage"
+  />
 </template>
 <script setup lang="ts">
-import { collection, limit, onSnapshot, orderBy, query } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  getDocs,
+  limit,
+  onSnapshot,
+  orderBy,
+  query,
+  setDoc,
+  startAfter,
+  updateDoc,
+} from "firebase/firestore";
 import { MESSAGE_LIMIT } from "~/shared/constant/constant";
 import { FIRESTORE_PATH } from "~/shared/constant/firebase-store";
 
@@ -24,41 +40,124 @@ const { $state } = useProfileStore();
 const navigatorTab = useNavigatorTabStore();
 const messageList = ref<TMessage[]>([]);
 const { $firebaseStore } = useNuxtApp();
+const message = ref<string>("");
 const { setScroll } = useScroll();
+const detectNewMessage = ref(false);
+const page = ref(0);
 
 const getAllMessage = async () => {
   const documentGroupId = `${navigatorTab.$state.currentTab.group?.sender.id}-${navigatorTab.$state.currentTab.group?.receiver.id}`;
-  const q = query(
+  const messageCollection = collection(
+    $firebaseStore,
+    FIRESTORE_PATH.chat_collection,
+    documentGroupId,
+    FIRESTORE_PATH.message_collection
+  );
+
+  console.log("page.value: ", page.value);
+
+  if (page.value > 0) {
+    const first = query(
+      messageCollection,
+      orderBy("created_at", "desc"),
+      limit(MESSAGE_LIMIT * page.value)
+    );
+    const documentSnapshots = await getDocs(first);
+    const lastVisible = documentSnapshots.docs[documentSnapshots.docs.length - 1];
+
+    const q = query(
+      messageCollection,
+      orderBy("created_at", "desc"),
+      startAfter(lastVisible),
+      limit(MESSAGE_LIMIT)
+    );
+
+    onSnapshot(q, (doc) => {
+      let tempSnapshot: TMessage[] = [];
+      doc.forEach((snapshot) => {
+        tempSnapshot.push(snapshot.data() as TMessage);
+      });
+      messageList.value = [...messageList.value, ...tempSnapshot];
+    });
+  } else {
+    const q = query(
+      messageCollection,
+      orderBy("created_at", "desc"),
+      limit(MESSAGE_LIMIT)
+    );
+
+    onSnapshot(q, (doc) => {
+      let tempSnapshot: TMessage[] = [];
+      doc.forEach((snapshot) => {
+        tempSnapshot.push(snapshot.data() as TMessage);
+      });
+      messageList.value = [...messageList.value, ...tempSnapshot];
+    });
+  }
+  page.value = page.value + 1;
+};
+
+const load = ({ done }: any) => {
+  console.log("Scrolling to load");
+  setTimeout(async () => {
+    await getAllMessage();
+    done("ok");
+  }, 2000);
+};
+
+const sendMessage = async () => {
+  if (!message.value.trim().length) {
+    return;
+  }
+  detectNewMessage.value = true;
+  const documentGroupId = navigatorTab.$state.currentTab.group?.group_id!;
+  const messageKey = doc(
     collection(
       $firebaseStore,
       FIRESTORE_PATH.chat_collection,
       documentGroupId,
       FIRESTORE_PATH.message_collection
-    ),
-    orderBy("created_at", "desc"),
-    limit(MESSAGE_LIMIT)
-  );
+    )
+  ).id;
 
-  onSnapshot(q, (doc) => {
-    let tempSnapshot: TMessage[] = [];
-    doc.forEach((snapshot) => {
-      tempSnapshot.push(snapshot.data() as TMessage);
-    });
-    messageList.value = tempSnapshot;
-  });
+  const dataMessage = {
+    message_id: messageKey,
+    user_id: $state.profile?.id || "",
+    content: message.value,
+    created_at: new Date().toString(),
+  };
+
+  try {
+    messageList.value = [dataMessage, ...messageList.value];
+    await setDoc(
+      doc(
+        $firebaseStore,
+        FIRESTORE_PATH.chat_collection,
+        documentGroupId,
+        FIRESTORE_PATH.message_collection,
+        messageKey
+      ),
+      dataMessage
+    );
+    await updateDoc(
+      doc($firebaseStore, FIRESTORE_PATH.chat_collection, documentGroupId),
+      { last_message: dataMessage }
+    );
+    console.log("send message success");
+  } catch (error) {
+    console.log("send message Error: ", error);
+  } finally {
+    detectNewMessage.value = false;
+    clearMessage();
+  }
 };
 
-const load = async ({ done }: any) => {
-  console.log("Scrolling to load");
-  done("ok");
+const clearMessage = () => {
+  message.value = "";
 };
 
-watch(messageList, () => {
+watch(detectNewMessage, () => {
   setScroll("message-list-scroll", 0, "instant");
-});
-
-watchEffect(() => {
-  getAllMessage();
 });
 </script>
 
--- components/main/panel/chat-panel.vue
+++ components/main/panel/chat-panel.vue
@@ -5,74 +5,17 @@
       :avatar="navigatorTab.$state.currentTab.group?.oppositeUser?.avatar || ''"
     />
     <MessageList />
-    <MessageInput
-      v-model="message"
-      @on-clear-message="clearMessage"
-      @on-send-message="sendMessage"
-    />
   </v-main>
 </template>
 
 <script lang="ts" setup>
-import { collection, doc, setDoc, updateDoc } from "firebase/firestore";
-import { FIRESTORE_PATH } from "~/shared/constant/firebase-store";
-
-const { $state } = useProfileStore();
 const navigatorTab = useNavigatorTabStore();
-const message = ref<string>("");
-const { $firebaseStore } = useNuxtApp();
+
 const fullName = computed(() => {
   const firstName = navigatorTab.$state.currentTab.group?.oppositeUser?.firstName;
   const lastName = navigatorTab.$state.currentTab.group?.oppositeUser?.lastName;
   return `${firstName} ${lastName}`;
 });
-
-const sendMessage = async () => {
-  if (!message.value.trim().length) {
-    return;
-  }
-  const documentGroupId = navigatorTab.$state.currentTab.group?.group_id!;
-  const messageKey = doc(
-    collection(
-      $firebaseStore,
-      FIRESTORE_PATH.chat_collection,
-      documentGroupId,
-      FIRESTORE_PATH.message_collection
-    )
-  ).id;
-
-  const dataMessage = {
-    message_id: messageKey,
-    user_id: $state.profile?.id,
-    content: message.value,
-    created_at: new Date().toString(),
-  };
-
-  try {
-    await setDoc(
-      doc(
-        $firebaseStore,
-        FIRESTORE_PATH.chat_collection,
-        documentGroupId,
-        FIRESTORE_PATH.message_collection,
-        messageKey
-      ),
-      dataMessage
-    );
-    await updateDoc(
-      doc($firebaseStore, FIRESTORE_PATH.chat_collection, documentGroupId),
-      { last_message: dataMessage }
-    );
-    console.log("send message success");
-  } catch (error) {
-    console.log("send message Error: ", error);
-  }
-  clearMessage();
-};
-
-const clearMessage = () => {
-  message.value = "";
-};
 </script>
 
 <style lang="scss" scoped>
