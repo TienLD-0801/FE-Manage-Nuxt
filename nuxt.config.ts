e882418e7cdd754b48798ecc48fba965858ce0d2
vfa-tienld <tienld@gmail.com>
2024-01-28 17:20:44 +0700

    remove dburl

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -43,7 +43,6 @@ export default defineNuxtConfig({
         apiKey: process.env.API_KEY,
         authDomain: process.env.AUTH_DOMAIN,
         projectId: process.env.PROJECT_ID,
-        databaseURL: process.env.DATABASE_URL,
         storageBucket: process.env.STORAGE_BUCKET,
         messagingSenderId: process.env.MESSAGING_SENDER_ID,
         appId: process.env.APP_ID,
